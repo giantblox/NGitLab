639e600412e068191f9eedc735c5bb674ab1912b
MysticBoy <mysticboy@live.com>
2018-04-01 23:04:50 +0800

    test

--- NGitLab/NGitLab.Tests/Config.cs
+++ NGitLab/NGitLab.Tests/Config.cs
@@ -1,10 +1,10 @@
 ﻿namespace NGitLab.Tests {
     public static class Config {
-        public const string ServiceUrl = "https://gitclub.cn";
+        public const string ServiceUrl = "https://gitclub.cn/";
         public const string Secret = "y1ZcmHSidM4bqwYzjFPU";
 
         public static GitLabClient Connect() {
-            return GitLabClient.Connect(ServiceUrl,"maikebing","285220Myh", Impl.Api.ApiVersion.V4_Oauth);
+            return GitLabClient.Connect(ServiceUrl, "gitlabtest", "yykissme", Impl.Api.ApiVersion.V4_Oauth);
         }
     }
 }
